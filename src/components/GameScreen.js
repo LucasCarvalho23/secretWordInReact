c4ff7be0e53ff9135842bb8358b4b7a1dbaf3bab
LucasCarvalho23 <lucasilva1990@hotmail.com>
2024-11-22 16:54:06 -0300

    layout hame

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -4,8 +4,26 @@ import "./GameScreen.css"
 const GameScreen = ({verifyLetter}) => {
   return (
     <div className='game'>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Finalizar jogo</button>
+      <p className='points'><span>Pontuação: 000</span></p>
+      <h1>Advinhe a palavra</h1>
+      <h3 className='tip'>Dica da palavra: <span>Dica...</span></h3>
+      <div className="wordContainer">
+        <span className='letter'>A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente advinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required/>
+          <button>Jogar</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:         
+          <span> a,</span>
+          <span>b,</span>
+        </p>
+      </div>
     </div>
   )
 }
